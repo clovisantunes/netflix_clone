92085c857f632404e4f456c8dac587860d604b65
Clovis antunes <clovissantanna@gmail.com>
2023-04-03 15:54:25 -0300

    criando pagina de login

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
 import './App.css';
-import ROW from './components/Row';
-import categories from './Tmdb'
-import Banner from './components/Banner';
-import Nav from './components/Nav';
+import Rotas from './components/Routes';
+
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <Banner />
-      {categories.map((category) => {
-        return<ROW 
-          key={category.name}
-          title={category.title}
-          path={category.path} 
-          isLarge={category.isLarge}
-          />;
-
-      })}
+    <div>
+      < Rotas />
     </div>
   );
 }
